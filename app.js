a4b03f426e585ea173a8deaa31b315e8c4e85968
Zahir Rafiq <cresarts@hotmail.com>
2020-09-29 12:04:37 +0100

    Task list add to storage part one

--- app.js
+++ app.js
@@ -12,17 +12,17 @@
 // const name = localStorage.getItem('name');
 // const age = localStorage.getItem('age');
 
-// // clear local storage
+// clear local storage
 // localStorage.clear();
 
 // console.log(name, age);
 
-document.querySelector('form').addEventListener('submit', function(e){
+document.querySelector('form').addEventListener('submit', function (e) {
   const task = document.getElementById('task').value;
 
   let tasks;
 
-  if(localStorage.getItem('tasks') === null) {
+  if (localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -31,14 +31,12 @@ document.querySelector('form').addEventListener('submit', function(e){
   tasks.push(task);
 
   localStorage.setItem('tasks', JSON.stringify(tasks));
-
   alert('Task saved');
-
   e.preventDefault();
 });
 
 const tasks = JSON.parse(localStorage.getItem('tasks'));
 
-tasks.forEach(function(task){
+tasks.forEach(function (task) {
   console.log(task);
 });
